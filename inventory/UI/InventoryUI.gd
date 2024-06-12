08f7352d4a1c70ff4a147543686759e9a6683d39
Oliver <131390934+GitForGood@users.noreply.github.com>
2024-06-12 15:17:30 +0200

    refactored ui elements of items to own folder

--- inventory/UI/InventoryUI.gd
+++ inventory/UI/InventoryUI.gd
@@ -13,7 +13,7 @@ func _ready():
 
 func insert_slots():
 	var grid = $NinePatchRect/Control/GridContainer
-	var slot_file = preload("res://inventory/InventoryUISlot.tscn")
+	var slot_file = preload("res://inventory/UI/InventoryUISlot.tscn")
 	for index in inventory.item_slots.size():
 		var slot = slot_file.instantiate()
 		slots.append(slot)
